d093f815c756d41c3a2f47bccf18cea8b316c8d1
Christian Barretto <67621305+NesKirusu@users.noreply.github.com>
2025-06-30 19:15:49 +0800

    Add files via upload

--- kevi_motors.sql
+++ kevi_motors.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2025 at 04:25 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Jun 30, 2025 at 01:15 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `orders` (
+  `orderID` int(11) NOT NULL,
   `order_date` date DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `item` varchar(255) DEFAULT NULL,
@@ -40,16 +41,12 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`order_date`, `email`, `item`, `cost`, `quantity`, `amount`) VALUES
-('0000-00-00', 'user@example.com', '0', 10000000, 2, 20000000),
-('0000-00-00', 'user@example.com', '                                    Tesla model C                                     ', 2100000, 3, 6300000),
-('0000-00-00', 'user@example.com', '                                    Tesla model C                                     ', 2100000, 3, 6300000),
-('0000-00-00', 'user@example.com', '                                    Tesla model X                                     ', 2000000, 1, 2000000),
-('0000-00-00', 'user@example.com', '                                    Tesla model C                                     ', 2100000, 1, 2100000),
-('0000-00-00', 'user@example.com', '                                    Tesla model Y                                     ', 2500000, 1, 2500000),
-('0000-00-00', 'user@example.com', '                                    Tesla model X                                     ', 2000000, 1, 2000000),
-('0000-00-00', 'user@example.com', 'Tesla model C', 2100000, 2, 4200000),
-('0000-00-00', 'user@example.com', 'Tesla model Y', 2500000, 2, 5000000);
+INSERT INTO `orders` (`orderID`, `order_date`, `email`, `item`, `cost`, `quantity`, `amount`) VALUES
+(1, '2025-06-30', 'customer@yahoo.com', 'Tesla model C', 2100000, 4, 8400000),
+(2, '2025-06-30', 'customer@yahoo.com', 'Tesla TRUCK', 10000000, 5, 50000000),
+(5, '2025-06-30', 'user@example.com', 'han', 3100000, 3, 9300000),
+(6, '2025-06-30', 'user@example.com', 'Tesla model Y', 2500000, 3, 7500000),
+(7, '2025-06-30', 'user@example.com', 'Tesla model C', 2100000, 3, 6300000);
 
 -- --------------------------------------------------------
 
@@ -58,26 +55,45 @@ INSERT INTO `orders` (`order_date`, `email`, `item`, `cost`, `quantity`, `amount
 --
 
 CREATE TABLE `users` (
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `email` varchar(256) NOT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `user_type` varchar(50) NOT NULL DEFAULT 'customer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`email`, `password`) VALUES
-('user@example.com', 'securepassword123');
+INSERT INTO `users` (`email`, `password`, `user_type`) VALUES
+('customer@yahoo.com', '$2y$10$HFHgUktkwQSEeDMZc9MuqutRUIo95VRGHcVF92cZNy1QlA0kdLUEq', 'customer'),
+('kevin@yahoo.com', '$2y$10$zxmQFBwCfw9p4jiKvgofw.XMxJkiecr9SHZrLk9CRR/vMkCrRJSPy', 'admin'),
+('user@example.com', '$2y$10$W8//Tmrtao/TXyRqd9GS2.6JGWvo6/MgofiCyqbQ9.x0PiXU1tYnK', 'customer');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
